87c2db1469a249eac15c216259fb34b6cc5e52b5
Marco Mendes <marcoandre@gmail.com>
2014-05-26 17:07:13 -0300

    Retirada de resolução

--- prg1-lista4a.py
+++ prg1-lista4a.py
@@ -7,69 +7,22 @@
 
 def ondernamento_contrario(lista):
     ''' Inverta a lista '''
-    nova_lista = []
-    
-    i = len(lista) - 1 
-    while i >= 0:
-        nova_lista.append(lista[i])
-        i = i - 1
-    return nova_lista
 
 def intercalamento_listas(lista1,lista2):
     ''' Usando 'lista1' e 'lista2', ambas do mesmo comprimento,
     crie uma nova lista composta pelo
     intercalamento entre as duas.'''
-    lista3 = []
-    i = 0
-    while i < len(lista1):
-        lista3.append(lista1[i])
-        lista3.append(lista2[i])    
-        i += 1
-    return lista3
 
 def im_pares(lista):
     ''' Separe em listas os impares e pares, dos inteiros da 'lista' '''
-    pares = []
-    impares = []
-    i = 0
-    while i < len(lista):
-        if lista[i] % 2 == 0: # se é par
-            pares.append(lista[i])
-        else: # se é impar
-            impares.append(lista[i])            
-        i += 1
-    return(pares,impares)
 
 def maior_menor(lista):
     ''' Calcule o maior e o menor numero da 'lista' '''
-    maior = lista[0] #recebe o 1o elemento
-    menor = lista[0]
-    i = 0
-    while i < len(lista):
-        if lista[i] > maior:
-            maior = lista[i]
-        if lista[i] < menor:
-            menor = lista[i]
-        i += 1
-
-    return (maior,menor)
 
 def dar_troco(valor_a_pagar, valor_em_dinheiro):
     ''' Calcule o troco numa lista com notas de 1,2,5,10,20,50 com sua 
     quantidade de notas sem considerar centavos
     ex: 1 e 10 retorna troco_notas_quantidade = [5,2] quantidade_notas = [1,2] '''
-    notas = [50,20,10,5,2,1]
-    troco = valor_em_dinheiro - valor_a_pagar
-    notas_quantidade_a_devolver = []
-    i = 0
-    while troco > 0:
-        if troco >= notas[i]:
-            quantidade_notas = int(troco / notas[i])
-            notas_quantidade_a_devolver.append((notas[i],quantidade_notas))
-            troco = troco % notas[i]
-        i += 1
-    return notas_quantidade_a_devolver
-
 
 # Área de testes: só mexa aqui se souber o que está fazendo!
 acertos = 0
@@ -94,24 +47,24 @@ def main():
     lista5 = [1,3,5,7,9]
     lista6 = [2,4,6,8,10]
 
-    #print(' Listas invertidas:')
-    #test(ondernamento_contrario(lista1), ([10,9,8,7,6,5,4,3,2,1]))
-    #test(ondernamento_contrario(lista2), ([0,-1]))
-    #test(ondernamento_contrario(lista3), ([-100.1,-100,100,2,10,0,-10]))
+    print(' Listas invertidas:')
+    test(ondernamento_contrario(lista1), ([10,9,8,7,6,5,4,3,2,1]))
+    test(ondernamento_contrario(lista2), ([0,-1]))
+    test(ondernamento_contrario(lista3), ([-100.1,-100,100,2,10,0,-10]))
 
-    #print(' Lista Intercalada:')
-    #test(intercalamento_listas(lista5,lista6), [1,2,3,4,5,6,7,8,9,10])
+    print(' Lista Intercalada:')
+    test(intercalamento_listas(lista5,lista6), [1,2,3,4,5,6,7,8,9,10])
 
-    #print(' Lista de pares e impares:')
-    #test(im_pares(lista1), ([2,4,6,8,10],[1,3,5,7,9]))
-    #test(im_pares(lista5), ([],[1,3,5,7,9]))
-    #test(im_pares(lista6), ([2,4,6,8,10],[]))
+    print(' Lista de pares e impares:')
+    test(im_pares(lista1), ([2,4,6,8,10],[1,3,5,7,9]))
+    test(im_pares(lista5), ([],[1,3,5,7,9]))
+    test(im_pares(lista6), ([2,4,6,8,10],[]))
 
-    #print(' Maior e o menor da lista:')
-    #test(maior_menor(lista1), (10,1))
-    #test(maior_menor(lista2), (0,-1))
-    #test(maior_menor(lista3), (100,-100.1))
-    #test(maior_menor(lista4), (-1,-10))
+    print(' Maior e o menor da lista:')
+    test(maior_menor(lista1), (10,1))
+    test(maior_menor(lista2), (0,-1))
+    test(maior_menor(lista3), (100,-100.1))
+    test(maior_menor(lista4), (-1,-10))
 
     print(' Troco do pagamento:')
     test(dar_troco(10,10), [])
